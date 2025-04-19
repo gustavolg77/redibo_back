5eff3375ad5fad460e69868b198dc8430ae2e3de
Luis Gustavo <luisgustavogarcia576@gmail.com>
2025-04-18 21:20:45 -0400

    1era version concluida+ datos estaticos

--- src/types/car.ts
+++ src/types/car.ts
@@ -1,14 +1,15 @@
 export interface Car {
-    id: number;
-    brand: string;
-    model: string;
-    year: number;
-    image: string;
-    status: string;
-    totalRentals: number;
-    totalUsageDays: number;
-    pricePerDay: number;
-    description: string;
-    features: string[];
-  }
-  
+  id: number;
+  brand: string;
+  model: string;
+  year: number;
+  image: string;
+  status: string;
+  totalRentals: number;
+  totalUsageDays: number;
+  pricePerDay: number;
+  description: string;
+  features: string[];
+  owner?: string;
+  color?: string;
+}
